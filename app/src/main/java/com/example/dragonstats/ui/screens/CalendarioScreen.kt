bd3aa07bca55259ac1cf179ffd21dc8130339ace
MarceloDetlefsen <marcelodetlefsen1a@gmail.com>
2025-09-09 08:05:26 -0600

    Se actualiza strings.xml para CalendarioScreen.kt

--- app/src/main/java/com/example/dragonstats/ui/screens/CalendarioScreen.kt
+++ app/src/main/java/com/example/dragonstats/ui/screens/CalendarioScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,7 +32,7 @@ fun CalendarioScreen(onPartidoClick: () -> Unit) {
     ) {
         // Header
         Text(
-            text = "Próximos encuentros",
+            text = stringResource(id = R.string.calendario_screen_title),
             color = Color.White,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
