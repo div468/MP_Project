25f259103e548f84843ba65d6a714386bbaedfaa
Jackelyn <jackelyngironvillacinda@gmail.com>
2025-11-09 19:02:38 -0600

    Implementacion de DataStore
    -Se implemento DataStore para persistencia de los equipos favoritos y no solo en la pantalla
    -Ordenamiento priorizando los favoritos, seguido de mayor punteo

--- app/src/main/java/com/example/dragonstats/ui/viewmodel/EquiposListadoViewModel.kt
+++ app/src/main/java/com/example/dragonstats/ui/viewmodel/EquiposListadoViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.dragonstats.ui.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.dragonstats.data.repository.EquipoRepository
 import com.example.dragonstats.data.model.Equipo
@@ -8,6 +9,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import com.example.dragonstats.data.local.FavoritosDataStore
 
 sealed class EquiposUiState {
     object Loading : EquiposUiState()
@@ -16,40 +18,60 @@ sealed class EquiposUiState {
 }
 
 class EquiposListadoViewModel(
+    application: Application
+) : AndroidViewModel(application){
     private val repository: EquipoRepository = EquipoRepository()
-) : ViewModel(){
+    private val favoritosDataStore = FavoritosDataStore(application)
 
     private val _uiState = MutableStateFlow<EquiposUiState>(EquiposUiState.Loading)
-
     val uiState: StateFlow<EquiposUiState> = _uiState.asStateFlow()
 
     private val _equiposFavoritos = MutableStateFlow<Set<String>>(emptySet())
     val equiposFavoritos: StateFlow<Set<String>> = _equiposFavoritos.asStateFlow()
 
+    private var equiposOriginal : List<Equipo> = emptyList()
     init {
         cargarEquipos()
+        viewFavoritos()
     }
 
+    private fun viewFavoritos(){
+        viewModelScope.launch {
+            favoritosDataStore.favoritosFlow.collect { favoritos ->
+                _equiposFavoritos.value = favoritos
+                reordenarEquipos()
+            }
+        }
+    }
     fun cargarEquipos(){
         viewModelScope.launch {
             _uiState.value = EquiposUiState.Loading
 
             val result = repository.getEquiposOrdenados()
-            _uiState.value = if (result.isSuccess){
-                EquiposUiState.Success(result.getOrNull() ?: emptyList())
+            if (result.isSuccess){
+                equiposOriginal = result.getOrNull() ?: emptyList()
+                reordenarEquipos()
             }else {
-                EquiposUiState.Error(result.exceptionOrNull()?.message ?: "Error desconocido")
+                _uiState.value = EquiposUiState.Error(
+                    result.exceptionOrNull()?.message ?: "Error desconocido")
             }
         }
     }
 
     fun toggleFavorito(nombreEquipo: String){
-        val favoritos = _equiposFavoritos.value.toMutableSet()
-        if (favoritos.contains(nombreEquipo)){
-            favoritos.remove(nombreEquipo)
-        }else{
-            favoritos.add(nombreEquipo)
+        viewModelScope.launch {
+            favoritosDataStore.toggleFavorito(nombreEquipo)
         }
-        _equiposFavoritos.value = favoritos
+    }
+
+    //Para reordenar los equipos para favoritos primero y despues ordenar por puntos
+    private fun reordenarEquipos(){
+        val favoritos = _equiposFavoritos.value
+        val equiposOrdenados = equiposOriginal.sortedWith (
+            compareByDescending <Equipo> {favoritos.contains(it.nombre)  }
+                .thenByDescending { it.puntos }
+                .thenByDescending { it.golDiferencia }
+        )
+        _uiState.value = EquiposUiState.Success(equiposOrdenados)
     }
 }
